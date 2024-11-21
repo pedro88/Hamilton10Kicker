f502080e8f5a4b8426de8bb4536c055c21269df1
pedro88 <sandrep_85@hotmail.com>
2024-11-21 12:25:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let recordedPlayers = []
+const addingNewPlayer = () => {
+const newPlayer = document.getElementById('newPlayer');
+recordedPlayers.push(newPlayer);
+console.log('new player :')
+console.log(newPlayer);
+console.log('recorded players :')
+console.log(recordedPlayers);
+}
+
+const newMatch = () => {
+
+}
