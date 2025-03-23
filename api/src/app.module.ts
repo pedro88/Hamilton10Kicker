225bdd0ba3658a1a26c2d3887f976ba5ef43692a
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-23 19:04:54 +0100

    Add db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true, // Permet d'utiliser les variables d'environnement globalement
+        envFilePath: '.env', // Chemin vers votre fichier .env
+    }),
+    TypeOrmModule.forRoot({
+        type: 'mariadb',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
