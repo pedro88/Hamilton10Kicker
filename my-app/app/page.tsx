24c6398be1daa7d90124070343b7578169e71b06
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-27 15:51:31 +0100

    Set the project

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 export default function Home() {
-  return (
-    <div>
-        <h1>HELLO</h1>
-    </div>
-  )
+
+    return (
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    );
 }
