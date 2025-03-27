4aa069dab987185f2808e5536482c40a984e0007
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-27 16:59:57 +0100

    Add a new font

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 
     return (
         <div>
-            <h1>Hey guys what's going on ?</h1>
+            <h1>PLAY WITH YOUR FRIEND WITH</h1>
         </div>
     );
 }
