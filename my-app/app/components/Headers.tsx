7dd2e5d61ef753826cd41a1370b95d64c3369c06
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-28 07:55:51 +0100

    Ajout de la page new-account

--- my-app/app/components/Headers.tsx
+++ my-app/app/components/Headers.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import LogoH10 from "../../public/img/logoKHWhite.svg";
+import Link from "next/link";
 
 const Headers = () => {
-  return (
-    <header className='flex justify-center'>
-        <Image src={LogoH10} alt='Logo'width={150}/>
-    </header>
-  )
-}
+    return (
+        <header className="flex justify-center">
+            <Link href="/">
+                <Image src={LogoH10} alt="Logo" width={150} />
+            </Link>
+        </header>
+    );
+};
 
-export default Headers
+export default Headers;
