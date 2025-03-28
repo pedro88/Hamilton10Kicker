ef16414d828aa33c078556c4bbd6c09d4c7830ab
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-28 10:42:53 +0100

    Add NewUserForm.tsx and a custom color in golobal.css

--- my-app/app/components/NewUserForm.tsx
+++ my-app/app/components/NewUserForm.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const NewUserForm = () => {
-  return (
-    <div>NewUserForm</div>
-  )
-}
+    return (
+        <form className="flex flex-col space-y-2 m-4">
+            <div className="flex items-center">
+                <label className="w-24">Username</label>
+                <input type="text" className="flex-1 border-2 rounded p-1" />
+            </div>
 
-export default NewUserForm
+            <div className="flex items-center">
+                <label className="w-24">Email</label>
+                <input type="email" className="flex-1 border-2 rounded p-1" />
+            </div>
+
+            <div className="flex items-center">
+                <label className="w-24">Password</label>
+                <input
+                    type="password"
+                    className="flex-1 border-2 rounded p-1"
+                />
+            </div>
+
+            <div className="flex items-center">
+                <label className="w-24">Country</label>
+                <input type="text" className="flex-1 border-2 rounded p-1" />
+            </div>
+            
+            <div className="flex justify-end mr-1 mt-2">
+                <button className="border-2 rounded-md bg-h10red w-20">Create</button>
+            </div>
+        </form>
+    );
+};
+
+export default NewUserForm;
