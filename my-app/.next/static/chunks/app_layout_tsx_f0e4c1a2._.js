773b159be10a6ef4eeb08e87bb188d669addf6cc
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-31 16:13:37 +0200

    add svg

---
+++ my-app/.next/static/chunks/app_layout_tsx_f0e4c1a2._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_f0e4c1a2._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_f0e4c1a2._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__8c3b39ae._.css",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0b1fe7dd._.js",
+    "static/chunks/node_modules_fe61c5f6._.js"
+  ],
+  "source": "dynamic"
+});
