9b6bb8f573cab645b93ae051961521ad91eb5d61
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-02 18:00:01 +0200

    Add start page

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -1,12 +1,14 @@
-import { CardWithForm } from "@/components/CardWithForm";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
     return (
         <div>
-            <h1 className="text-4xl text-center mt-5 mb-10">IT&#39;S TIME TO PLAY !</h1>
+            <h1 className="text-4xl text-center mt-5 mb-10">TOP SCORE</h1>
             <div className="flex justify-center">
-                <CardWithForm />
+                <Link href={"/start"}><Button>PLAY</Button></Link>
             </div>
         </div>
     );
