af7b87bfbed44b10a1b9c39cc25603a4a3837385
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-02 18:17:29 +0200

    Add table component

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -1,5 +1,14 @@
-
 import { Button } from "@/components/ui/button";
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableFooter,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableCaption,
+} from "@/components/ui/table";
 import Link from "next/link";
 import React from "react";
 
@@ -8,7 +17,9 @@ const HomePage = () => {
         <div>
             <h1 className="text-4xl text-center mt-5 mb-10">TOP SCORE</h1>
             <div className="flex justify-center">
-                <Link href={"/start"}><Button>PLAY</Button></Link>
+                <Link href={"/start"}>
+                    <Button>PLAY</Button>
+                </Link>
             </div>
         </div>
     );
