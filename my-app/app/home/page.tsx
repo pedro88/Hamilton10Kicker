b9fa44ad4be0c05813d76fddefd5537a73829b6b
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-05 11:18:40 +0200

    Add a table of score

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -1,14 +1,5 @@
+import ScoreTable from "@/components/ScoreTable";
 import { Button } from "@/components/ui/button";
-import {
-    Table,
-    TableHeader,
-    TableBody,
-    TableFooter,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableCaption,
-} from "@/components/ui/table";
 import Link from "next/link";
 import React from "react";
 
@@ -16,10 +7,11 @@ const HomePage = () => {
     return (
         <div>
             <h1 className="text-4xl text-center mt-5 mb-10">TOP SCORE</h1>
-            <div className="flex justify-center">
+            <div className="flex flex-col items-center">
                 <Link href={"/start"}>
                     <Button>PLAY</Button>
                 </Link>
+                <ScoreTable/>
             </div>
         </div>
     );
