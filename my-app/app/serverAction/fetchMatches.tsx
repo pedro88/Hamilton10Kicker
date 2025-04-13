595016ef75e1d76bcfa5a0104f631833d4845769
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-13 19:25:37 +0200

    Make server action achitechture for api call

---
+++ my-app/app/serverAction/fetchMatches.tsx
@@ -0,0 +1,133 @@
+// 'use server'
+
+export function fetchSoloMatches() {
+
+    const soloScore = [
+        {
+            id: 2,
+            player1: "Michel",
+            player2: "Pedro",
+            score1: 11,
+            score2: 9,
+            winner: "Michel",
+            created_at: "5/3/24",
+        },
+        {
+            id: 3,
+            player1: "Lucas",
+            player2: "Anna",
+            score1: 10,
+            score2: 8,
+            winner: "Lucas",
+            created_at: "7/3/24",
+        },
+        {
+            id: 4,
+            player1: "Thomas",
+            player2: "Sophie",
+            score1: 7,
+            score2: 10,
+            winner: "Sophie",
+            created_at: "8/3/24",
+        },
+        {
+            id: 5,
+            player1: "Marco",
+            player2: "Laura",
+            score1: 9,
+            score2: 10,
+            winner: "Laura",
+            created_at: "10/3/24",
+        },
+        {
+            id: 6,
+            player1: "Michel",
+            player2: "Lucas",
+            score1: 11,
+            score2: 9,
+            winner: "Michel",
+            created_at: "12/3/24",
+        },
+        {
+            id: 7,
+            player1: "Pedro",
+            player2: "Anna",
+            score1: 8,
+            score2: 10,
+            winner: "Anna",
+            created_at: "14/3/24",
+        },
+        {
+            id: 8,
+            player1: "Thomas",
+            player2: "Marco",
+            score1: 10,
+            score2: 6,
+            winner: "Thomas",
+            created_at: "16/3/24",
+        },
+        {
+            id: 17,
+            player1: "Thomas",
+            player2: "Michel",
+            score1: 10,
+            score2: 11,
+            winner: "Michel",
+            created_at: "12/4/24",
+        },
+        {
+            id: 18,
+            player1: "Laura",
+            player2: "Anna",
+            score1: 10,
+            score2: 3,
+            winner: "Laura",
+            created_at: "15/4/24",
+        },
+        {
+            id: 19,
+            player1: "Marco",
+            player2: "Sophie",
+            score1: 10,
+            score2: 9,
+            winner: "Marco",
+            created_at: "20/4/24",
+        },
+        {
+            id: 20,
+            player1: "Pedro",
+            player2: "Thomas",
+            score1: 8,
+            score2: 10,
+            winner: "Thomas",
+            created_at: "25/4/24",
+        },
+        {
+            id: 21,
+            player1: "Lucas",
+            player2: "Laura",
+            score1: 11,
+            score2: 10,
+            winner: "Lucas",
+            created_at: "1/5/24",
+        },
+        {
+            id: 22,
+            player1: "Michel",
+            player2: "Sophie",
+            score1: 10,
+            score2: 7,
+            winner: "Michel",
+            created_at: "5/5/24",
+        },
+    ];
+
+    return soloScore;
+
+}
+
+export function fatchTeamMatches() {
+
+}
+
+export default fatchTeamMatches
