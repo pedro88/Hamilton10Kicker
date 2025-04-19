1937cb50a3bed34c0d9e5310c7a32f35eda1b408
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-19 13:58:13 +0200

    add progress bar in profile overview

--- my-app/components/ProfileOverview.tsx
+++ my-app/components/ProfileOverview.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { FC } from "react";
 import quentinjpg from "@/public/img/quentin.jpg";
 import { User } from "@/utils/type";
+import { Progress } from "./ui/progress";
 
 type ProfileOverviewProps = {
     FetchUserProfile: User;
@@ -11,7 +12,7 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ FetchUserProfile }) => {
     const user = FetchUserProfile;
     return (
         <div className="flex flex-row items-center">
-            <div className="relative w-20 h-20 overflow-hidden rounded-full mr-15">
+            <div className="relative w-25 h-25 overflow-hidden rounded-full mr-10">
                 <Image
                     src={quentinjpg}
                     alt="Photo de profil"
@@ -19,15 +20,16 @@ const ProfileOverview: FC<ProfileOverviewProps> = ({ FetchUserProfile }) => {
                     className="object-cover"
                 />
             </div>
-            <div className="font-bold">
+            <div className="text-xl font-semibold">
                 <p>{user.username}</p>
-                <p>Level : {user.level}</p>
                 <p>
                     Solo W/L : {user.won_game}/{user.lost_game}
                 </p>
                 <p>
                     Team W/L : {user.won_team_game}/{user.lost_team_game}
                 </p>
+                <p>Level : {user.level}</p>
+                <Progress value={user.xp}/>
             </div>
         </div>
     );
