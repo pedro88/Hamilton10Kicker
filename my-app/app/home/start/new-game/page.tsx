e8c7742d1574ae6b8f255b77cc7c7a4c13fbecee
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-22 07:53:56 +0200

    add a new page newgame

---
+++ my-app/app/home/start/new-game/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewGamePage = () => {
+    return (
+        <div>
+            <h1>THIS IS THE NEW GAME PAGE</h1>
+        </div>
+    );
+};
+
+export default NewGamePage;
