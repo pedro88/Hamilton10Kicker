dbe9e1c3f4fec38613ef5249ac656bfa2406fe99
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-27 17:52:23 +0200

    implement verification on all field for the register

---
+++ my-app/components/molecules/SelectCountry.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { FriendUserList } from "@/lib/type";
+import { useState, FC } from "react";
+
+type SelectPlayerProps = {
+    FetchFriendWithUserId: FriendUserList[]
+    onPlayerSelect: (name: string) => void;
+}
+
+const SelectPlayer: FC<SelectPlayerProps> = ({ 
+    FetchFriendWithUserId,
+    onPlayerSelect}) => {
+
+    const [player, setPlayer] = useState("");
+    const [open, setOpen] = useState(false);
+    const friends = FetchFriendWithUserId;
+
+    const handleSelect = (name: string) => {
+        setPlayer(name);
+        onPlayerSelect(name);
+        setOpen(false);
+    };
+
+    return (
+        <DropdownMenu open={open} onOpenChange={setOpen}>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">{player ? player : "Select player"}</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                { friends && (
+                    friends.map((f) => (
+                        <DropdownMenuLabel 
+                            key={f.username} 
+                            className="hover:bg-gray-50" 
+                            onClick={() => handleSelect(f.username)}
+                        >
+                            {f.username}
+                        </DropdownMenuLabel>
+                    ))
+                )}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
+export default SelectPlayer;
