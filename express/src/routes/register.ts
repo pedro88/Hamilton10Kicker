304af00f8951c55016c8cb3b840623173cd3d738
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-28 07:55:55 +0200

    Create a beginning of password hasher to register.ts

--- express/src/routes/register.ts
+++ express/src/routes/register.ts
@@ -1,7 +1,18 @@
-import express, { Request, Response } from 'express';
-
+import express, { Request, Response } from "express";
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
+// I'm stopped here. The doc
+// https://www.freecodecamp.org/news/how-to-hash-passwords-with-bcrypt-in-nodejs/
 
+router.post("/register", (res: Response, req: Request) => {
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, (err: string, salt: string) => {
+        if (err) {
+            console.log("No salt... : ", err);
+            return;
+        }
+    });
+});
 
 module.exports = router;
