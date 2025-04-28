e2ee1f2021a521d89fe93bc325adce6c9bf20b59
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-28 18:43:49 +0200

    Add a post test

--- express/src/routes/source.ts
+++ express/src/routes/source.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
+router.use(express.json());
 
 router.get("/", (req: Request, res: Response) => {
     res.send({message: "The api work well !"});
+})
+
+router.post("/", (req: Request, res: Response) => {  
+    res.status(201).send(req.body);
 })
 
 module.exports = router;
