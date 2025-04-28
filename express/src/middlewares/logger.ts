9d23bd9a49fa96580c6cce1d9032bfdfeeaab05b
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-28 18:51:04 +0200

    add a middleware test

---
+++ express/src/middlewares/logger.ts
@@ -0,0 +1,9 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+const router = express.Router();
+router.use(express.json());
+
+module.exports = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next(); 
+};
