caa6f73b331c5eaa71c9aeb0928181478a474c2f
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-28 08:44:40 +0200

    hash password

--- express/src/routes/register.ts
+++ express/src/routes/register.ts
@@ -5,14 +5,28 @@ const router = express.Router();
 // I'm stopped here. The doc
 // https://www.freecodecamp.org/news/how-to-hash-passwords-with-bcrypt-in-nodejs/
 
-router.post("/register", (res: Response, req: Request) => {
+router.post("/register", async (res: Response, req: Request) => {
     const saltRounds = 10;
-    bcrypt.genSalt(saltRounds, (err: string, salt: string) => {
+    const userPassword = "12345678";
+    let salty;
+    await bcrypt.genSalt(saltRounds, (err: string, salt: string) => {
         if (err) {
             console.log("No salt... : ", err);
             return;
         }
+        
+        bcrypt.hash(userPassword, salt, (error: string, hash: string) => {
+            if (error) {
+                console.log(error)
+                return;
+            }
+        
+        // Hashing successful, 'hash' contains the hashed password
+        console.log('Hashed password:', hash);
+        });
+
     });
+    
 });
 
 module.exports = router;
--- express/src/routes/source.ts
+++ express/src/routes/source.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.send(["The api work well !"]);
+    res.send({message: "The api work well !"});
 })
 
 module.exports = router;
