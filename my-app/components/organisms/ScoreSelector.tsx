d191aa9fe56aa33bb9ccabe432d1318f4c686883
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-29 18:00:14 +0200

    configure the score Selector

--- my-app/components/organisms/ScoreSelector.tsx
+++ my-app/components/organisms/ScoreSelector.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import {
     Drawer,
@@ -11,18 +11,25 @@ import {
     DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const ScoreSelector = () => {
     return (
         <Drawer>
-            <DrawerTrigger>Open</DrawerTrigger>
+            <DrawerTrigger>Play !</DrawerTrigger>
             <DrawerContent>
                 <DrawerHeader>
-                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                    <DrawerTitle>Put your score !</DrawerTitle>
                     <DrawerDescription>
                         This action cannot be undone.
                     </DrawerDescription>
                 </DrawerHeader>
+                <div className="flex flex-row justify-center">
+                    <p className="mt-1 mr-3" >Mehdi</p>
+                    <Input type="number" className="w-20 mr-8" />
+                    <p className="mr-3">Hugo</p>
+                    <Input type="number" className="w-20" />
+                </div>
                 <DrawerFooter>
                     <Button>Submit</Button>
                     <DrawerClose>
