afc422c1a9e93a708d124c17c601f8e9deaed7a0
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-10 16:58:08 +0200

    Create a new table users

---
+++ express/src/entity/Users.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    username!: string;
+
+    @Column()
+    password!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    country!: string;
+
+    @Column()
+    level: number = 0;
+
+    @Column()
+    xp: number = 0;
+
+    @Column()
+    wongame: number = 0;
+
+    @Column()
+    lostgame: number = 0;
+
+    @Column()
+    wonteamgame: number = 0;
+
+    @Column()
+    lostteamgame: number = 0;
+
+    @Column()
+    createdat: string = Date();
+}
